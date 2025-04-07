338bfd55121f109b2856090bc177324673808deb
lemon <103284423+useeryous@users.noreply.github.com>
2025-04-07 01:03:12 +0000

    modified:   3d-bl0cks/src/App.vue 	new file:   3d-bl0cks/src/components/main.vue 	new file:   3d-bl0cks/src/components/topbar.vue

--- 3d-bl0cks/src/App.vue
+++ 3d-bl0cks/src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--<img alt="Vue logo" src="./assets/logo.png">-->
+  <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import main from './components/main.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    main
+    //HelloWorld
   }
 }
 </script>
---
+++ 3d-bl0cks/src/components/main.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="main">
+        <canvas>
+            
+        </canvas>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: main
+    }
+</script>
+
+<style>
+
+</style>
