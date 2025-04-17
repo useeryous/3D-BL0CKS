129b772497d77baf920a35c726781089a55ccf94
lemon <103284423+useeryous@users.noreply.github.com>
2025-04-17 17:37:54 +0000

    modified:   public/index.html 	modified:   public/scripts/script.js 	modified:   public/styles/style.css

---
+++ public/scripts/script.js
@@ -0,0 +1,5 @@
+let allBtn = document.getElementById('allBtn').addEventListener('click');
+let gamesBtn = document.getElementById('gamesBtn').addEventListener('click');
+let appsBtn = document.getElementById('appsBtn').addEventListener('click');
+let NSFWBtn = document.getElementById('NSFWBtn').addEventListener('click');
+let otherBtn = document.getElementById('otherBtn').addEventListener('click');
